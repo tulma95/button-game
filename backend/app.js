8269a17a38ee99cea3335270a3357a183e9080c7
Riku Honkanen <riku.honkanen9595@gmail.com>
2020-01-09 18:56:55 +0200

    Try to fix heroku

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const playersRouter = require('./controllers/players')
 const buttonRouter = require('./controllers/button')
 const bodyParser = require('body-parser')
@@ -7,17 +8,19 @@ const cors = require('cors')
 app.use(cors())
 require('dotenv').config()
 
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true
 }).catch(error => console.log(error))
 
 mongoose.set('useCreateIndex', true)
 
-
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 app.use(bodyParser.json())
+
+app.use('/', express.static('./frontend/build'))
 app.use('/api/players', playersRouter)
 app.use('/api/button', buttonRouter)
 
